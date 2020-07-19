7c97c7853864d12a5e543cdf21f0bc2a336b8d80
WindyOktia <win.oktia@gmail.com>
2020-07-19 14:38:33 +0700

    add errors info and response data

--- application/views/admin/raw.php
+++ application/views/admin/raw.php
@@ -1,32 +1,9 @@
-<!-- <div class="card card-body">
-    <legend>Pilih data untuk Export Excel</legend>
-    <form action="">
-        <div class="row">
-            <div class="col">
-                <label for="">Pilih Tabel</label>
-                <select name="tabel" id="" class="form-control">
-                    <option value="0">Semua Tabel</option>
-                    <option value="1">Saham</option>
-                    <option value="2">Bank</option>
-                </select>
-            </div>
-            <div class="col">
-                <label for="">Pilih Sesi</label>
-                <select name="sesi" id="" class="form-control">
-                    <option value="0">Semua Sesi</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                </select>
-            </div>
-        </div>
-        <button type="submit" class="btn btn-success btn-sm mt-3 float-right">Export</button>
-    </form>
-</div> -->
 
 <a href="<?=base_url('export/export')?>" class="btn btn-sm btn-success">Excel Export</a>
+
+<div class="card card-body mt-3 ">
+    <h4><b><span class="badge-danger">Info</span></b></h4>
+    <h5>Apabila ditemui error seperti dibawah ini ketika melakukan export data, dapat diatasi dengan cara klik tombol <i>back</i> pada browser lalu lakukan export ulang</h5>
+    
+    <img src="<?=base_url('assets/images/errors.png')?>" style="width:100%" alt="">
+</div>
